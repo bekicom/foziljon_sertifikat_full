7b75d835b8273c9e2ef9ec8a05da852289ae9da3
bekicom <adelkauz6@gmail.com>
2026-08-26 13:49:01 +0500

    Fix diploma background in mobile PDF

--- frontend_edu/src/sertificate/components/certificates/Dip/Dip.js
+++ frontend_edu/src/sertificate/components/certificates/Dip/Dip.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import './style.css'
 import QRCode from "react-qr-code";
 import { AuthContext } from "../../../context/AuthContext";
+import diplomaBackground from './2024-07-03 12.18.17.jpg';
 
 const DipCertificat = React.forwardRef((props, ref) => {
   const { URL } = useContext(AuthContext);
@@ -16,8 +17,12 @@ const DipCertificat = React.forwardRef((props, ref) => {
   } = props.obj;
   return (
     <div className="certificat_ContainerEng" ref={ref}>
-
-
+      <img
+        className="diplomaBackground"
+        src={diplomaBackground}
+        alt=""
+        aria-hidden="true"
+      />
       <div className="containerDip">
 
         <h3 className="engID4">
